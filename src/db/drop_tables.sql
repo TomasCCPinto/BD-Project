5b575635060a1109334da60941517c25bab24a41
JoaoESmoreira <jesmoreira2010@gmail.com>
2022-04-21 15:35:18 +0100

    add new scripts to create and add tables

---
+++ src/db/drop_tables.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS buyer_campaign;      -- check
+DROP TABLE IF EXISTS coupon;              -- check
+DROP TABLE IF EXISTS forum_notifications; -- check
+DROP TABLE IF EXISTS quantity;            -- check
+DROP TABLE IF EXISTS to_order;            -- check
+DROP TABLE IF EXISTS campaign;            -- check
+DROP TABLE IF EXISTS administrator;       -- check
+DROP TABLE IF EXISTS buyer;               -- check
+DROP TABLE IF EXISTS features;            -- check
+DROP TABLE IF EXISTS history;             -- check
+DROP TABLE IF EXISTS rating;              -- check
+DROP TABLE IF EXISTS product;             -- check
+DROP TABLE IF EXISTS seller;              -- check
+DROP TABLE IF EXISTS customer;            -- check
