211d0415d2e81e4eb457a5f8236affdefb18ceb6
JoaoESmoreira <jesmoreira2010@gmail.com>
2022-04-22 11:03:35 +0100

    make a register

--- src/app/novo.py
+++ src/app/novo.py
@@ -1,3 +1,4 @@
+from email import message
 from flask import Flask, jsonify, request
 from werkzeug.security import generate_password_hash, check_password_hash
 
@@ -16,25 +17,32 @@ app = Flask(__name__)
 def root():
     return {"message" : "Welcome to the mother fucker shop program!"}
 
+@app.route('/<name>')
+def root_name(name):
+    return {"message" : f"Welcome to the mother fucker shop program {name}!"}
+
+
 def get_token(cursor):
     for row in cursor:
         return row[5]
 
     return ""
 
-@app.route('/login/<string:name>/<string:password1>')
-def get_login(name, password1):
-    if name and password1:
-        conn = connetion_db()
+@app.route('/login/<string:name>/<string:password>')
+def get_login(name, password):
+    if name and password:
+        conn   = connetion_db()
         cursor = conn.cursor()
+        sql    = f"SELECT * FROM customer WHERE name like '{name}'"
         cursor.execute(sql)
-        sql = f"SELECT * FROM customer WHERE name like '{name}'"
 
         my_token = get_token(cursor)
+        cursor.close()
+        conn.close()
 
         if my_token != "":
 
-            if check_password_hash(my_token, password1):
+            if check_password_hash(my_token, password):
                 return {"pass": "BEM-VINDO"}
             else:
                 return {"pass" : "A pass nao consta na base de dados"}
@@ -43,6 +51,47 @@ def get_login(name, password1):
 
     return jsonify({"message" : "please enter a name or pass"})
 
+
+def get_register(cursor, id_user, name, nif, adress, mail, password):
+    if cursor.rowcount == 0:
+        token = generate_password_hash(password=password)
+
+        connetion = connetion_db()
+        cursor    = connetion.cursor()
+        sql       = f"INSERT INTO customer (id_user, name, nif, adress, email, password) VALUES ('{id_user}', '{name}', '{nif}', '{adress}', '{mail}', '{token}')"
+        cursor.execute(sql)
+        connetion.commit()
+        cursor.close()
+        connetion.close()
+
+        return True
+    else:
+        return False
+
+
+@app.route('/reg/<string:name>/<string:nif>/<string:addr>/<string:email>/<string:my_p>')
+def set_register(name, nif, addr, email, my_p):
+    if name and nif and addr and email and my_p:
+        connetion = connetion_db()
+        cursor    = connetion.cursor()
+        sql       = f"SELECT * FROM customer WHERE name like '{name}'"
+
+        cursor.execute(sql)
+        cursor.close()
+
+        if get_register(cursor, 33, name, nif, addr, email, my_p):    
+            return {"menssage": "CONGRATS! seja bem vindo"}
+        else:
+            return {'message': "User ja registado"}
+    return {'message': "ola"}
+
+"""@app.route('/reg/<string:name>')
+def set_register(name):
+    if name:
+        return {"message" : name}
+    return {"message"}"""
+
+# http://127.0.0.1:5000/register/tomas/3542198/rua%20do%20pinhal/tftlord@qualquer.coisa/seila
 
 if __name__ == "__main__":
     app.run(debug=True)
