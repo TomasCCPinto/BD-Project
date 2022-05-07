42dea9c8e3184f931f2a045dbf4a24250b7e8be2
TC121121 <tomascerveira10@gmail.com>
2022-05-07 17:58:38 +0100

    add and update product

--- flask_factory/product.py
+++ flask_factory/product.py
@@ -19,41 +19,66 @@ def add_product():
             "status": GET_ERROR_CODE,
             "error": "No json"
         })
-    if "name" not in args or "description" not in args or "stock" not in args or "price" not in args or "seller_customer_id_user" not in args:
+    if "type" not in args or "description" not in args or "height" not in args or "weight" not in args or "colour" not in args or "stock" not in args or "price" not in args or "seller_customer_id_user" not in args:
 
         return jsonify({
             "status": GET_ERROR_CODE,
             "error": "Wrong parameters"
         })
     
-    name     = args["name"]
+    type    = args["type"]
     description      = args["description"]
+    height  = args["height"]
+    weight = args["weight"]
+    colour = args["colour"]
     stock   = args["stock"]
     price    = args["price"]
     seller_customer_id_user  = args["seller_customer_id_user"]
 
-    query   = f"SELECT * FROM product WHERE name like '{name}' and seller_customer_id_user = {seller_customer_id_user}"
     message = {}
 
     try:
+
+        conn   = db.get_data_base()
+        cursor = conn.cursor()
+        query = f"INSERT INTO product (type, description,height,weight,colour, stock, price,seller_customer_id_user ) VALUES ('{type}','{description}',{height},{weight},'{colour}',{stock},{price},{seller_customer_id_user}); SELECT currval('product_id_prod_seq');"
+        cursor.execute(query)
+        row   = cursor.fetchall()[0]
+
+        cursor.close()
+        conn.commit()
+        
+       
+        cursor = conn.cursor()
+        query2 = "UPDATE product SET product_id_prod = ((select max(product_id_prod) from product)+1) where product_id_prod is null;"
+        cursor.execute(query2)
+        
+        cursor.close()
+        conn.commit()
+        
+        
+        conn.close()
+        message["status"]  = SUCCESS_CODE
+        message["message"] = "Product added successfully"
+        message["results"] = row[0]
+
+        '''
+        
         with db.get_data_base() as conn:
-            with conn.cursor() as cursor:
-                print("aquii")
+            with conn.cursor() as cursor:                
+                query = f"INSERT INTO product (type, description,height,weight,colour, stock, price,seller_customer_id_user ) VALUES ('{type}','{description}',{height},{weight},'{colour}',{stock},{price},{seller_customer_id_user}); SELECT currval('product_id_prod_seq');"
+                print("aaa1")
                 cursor.execute(query)
-
-                if cursor.rowcount == 0:
-                    query = f"INSERT INTO product (name, description, stock, price, seller_customer_id_user ) VALUES ('{name}', '{description}', {stock}, {price}, {seller_customer_id_user}); SELECT currval('product_id_prod_seq');"
-
-                    cursor.execute(query)
-                    row   = cursor.fetchall()[0]
-
-                    message["status"]  = SUCCESS_CODE
-                    message["message"] = "Product added successfully"
-                    message["results"] = row[0]
-
-                else:
-                    message["status"] = GET_ERROR_CODE
-                    message["error"]  = "product already registed"
+                print("aaa")
+                query2 = "UPDATE product SET product_id_prod = ((select max(product_id_prod) from product)+1) where product_id_prod is null;"
+                cursor.execute(query2)
+                print("222")
+                row   = cursor.fetchall()[0]
+                message["status"]  = SUCCESS_CODE
+                message["message"] = "Product added successfully"
+                message["results"] = row[0]
+        '''
+            
 
     except:
         return jsonify({
@@ -77,7 +102,7 @@ def refresh_product(prod_id):
             "error": "No json"
         })
 
-    if "description" not in args or "price" not in args:
+    if  "description" not in args or "height" not in args or "weight" not in args or "colour" not in args or "price" not in args :
 
         return jsonify({
             "status": GET_ERROR_CODE,
@@ -86,11 +111,15 @@ def refresh_product(prod_id):
     
     description = args["description"]
     price = args["price"]
+    height  = args["height"]
+    weight = args["weight"]
+    colour = args["colour"]
 
 
     query   = f"SELECT * FROM product WHERE id_prod = {prod_id}"
     message = {}
 
+    #RETORNAR ID????
     try:
         with db.get_data_base() as conn:
             with conn.cursor() as cursor:
@@ -102,11 +131,11 @@ def refresh_product(prod_id):
 
                 else:
                     #ERRO AQUI PROBABLY
-                    query = f"UPDATE product SET description = '{description}', price = {price} WHERE id_prod = {prod_id};"
+                    query = f"""INSERT INTO product (type, description,height,weight,colour, stock, price,product_id_prod,seller_customer_id_user )
+                             VALUES ((select type from product where id_prod = {prod_id}),'{description}',{height},{weight},'{colour}',(select stock from product where id_prod = {prod_id}),{price},{prod_id},(select seller_customer_id_user from product where id_prod = {prod_id})); SELECT currval('product_id_prod_seq');"""
 
                     cursor.execute(query)
-                    row   = cursor.fetchall()[0]
-
+                    row   = cursor.fetchall()[0]                 
                     message["status"]  = SUCCESS_CODE
                     message["message"] = "Product values updated"
                     message["results"] = row[0]
